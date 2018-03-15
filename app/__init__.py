ac2259ee1e917be73f2f17bf479463b151907742
James-Laptop <jdkelly1985@gmail.com>
2018-03-15 10:07:48 +0000

    restructured main

--- app/__init__.py
+++ app/__init__.py
@@ -27,6 +27,9 @@ app.register_blueprint(errors_bp)
 from app.auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
+
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -55,4 +58,4 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 
-from app import routes, models
+from app import models
